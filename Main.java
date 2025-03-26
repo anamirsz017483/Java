32ba51c7a4a43e4c73db19c07b0c41d19df8c786
anamirsz017483 <anarosa.miranda1a@gmail.com>
2025-03-26 10:41:22 -0300

    meu primeiro projeto java

---
+++ Main.java
@@ -0,0 +1,9 @@
+/******************************************************************************
+ meu primeiro programa - Hello world 
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
