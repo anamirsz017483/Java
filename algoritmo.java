7912db6ae18e3267fd9734a21fbd18d59cf01648
anamirsz017483 <anarosa.miranda1a@gmail.com>
2025-03-31 13:56:58 -0300

    programação de algoritmo

---
+++ algoritmo.java
@@ -0,0 +1,18 @@
+/*****************************************************************************
+ * 
+Escreva um programa que exiba 10 vezes a mensagem “Eu gosto de estudar Algoritmos!”.
+
+*******************************************************************************/
+import java.util.Scanner ;
+public class algoritmo
+{
+    public static void main (String [] args ) {
+ 
+
+         for (int i = 1; i <=10; i++) {
+            System.out.println("eu gosto de estudar algoritmo! :");
+        }
+        }
+        
+    }
+
