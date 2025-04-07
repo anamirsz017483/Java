c3b694111a412106ea76ffb79f98f94eafb10c48
anamirsz017483 <anarosa.miranda1a@gmail.com>
2025-04-07 15:06:29 -0300

    programação contador

---
+++ exemplodowhile.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class exemplodowhile 
+{
+	public static void main(String[] args) {
+	    int contador =0;
+	    
+	    do{
+	        System.out.println("contador:");
+	        contador++;
+	    }while (contador <= 5);
+	  }
+}	  
