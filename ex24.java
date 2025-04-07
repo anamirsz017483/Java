1b3a464a6b340eb70114a4e5e3d59130f828ba18
anamirsz017483 <anarosa.miranda1a@gmail.com>
2025-04-07 10:42:18 -0300

    programação de soma os numeros de 1 a 10

---
+++ ex24.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+soma de numeros de 1 a 10 = 1+2+3+4...10
+
+*******************************************************************************/
+public class ex24
+{
+	public static void main(String[] args) {
+	int soma = 0;
+	int numero =1;
+	
+	while (numero <=10) {
+	    soma += numero;
+	    numero ++;
+	}
+	System.out.println("a soma dos numeros de 1 a 10 é:" + soma);
+	
+	}
+}
