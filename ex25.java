4992f5b925058664b52836157e6a7d5d2d812519
anamirsz017483 <anarosa.miranda1a@gmail.com>
2025-04-07 11:01:17 -0300

    programação que mostra a tela dos numeros de 0 a 100

---
+++ ex25.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+Faça um programa, utilizando while, que mostre na tela os números de 0 a 100.
+
+*******************************************************************************/
+import java.util.Scanner; 
+public class ex25 
+{
+	public static void main(String[] args) {
+	    int i =0;
+	    
+	    while (i <= 100) {
+	        System.out.println(i);
+	        i++;
+	    }
+  }
+}
