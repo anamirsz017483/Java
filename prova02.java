fd0ae0b9c7cfded7f13e5cbba6ee7e1e7821f9cb
anamirsz017483 <anarosa.miranda1a@gmail.com>
2025-05-05 11:07:52 -0300

    programa que imprima numero de 1 a 10 usando o for

---
+++ prova02.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+Escreva um programa em Java que imprima os números de 1 a 10 usando o laço for.
+*******************************************************************************/
+import java.util.Scanner; 
+public class prova02
+{
+    public static void main(String[] args) {
+    
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
